b85cb0e24081aaa32cd4c38d3850f7db1e4d6bb8
Steven Maitre <73316151+StevenM91@users.noreply.github.com>
2022-09-26 12:04:49 +0200

    logique d'affiche conditionnelle quand on est connecter, logique de déconnexion crée

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { useState } from "react";
 import ConnectModal from "./components/ConnectModal";
 import "./styles/index.scss";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./utils/firebase.config";
+import CreatePost from "./components/CreatePost";
 
 function App() {
+  //on crée une variable pour stocker nos donnée utilisateur
+  const [user, setUser] = useState(null);
+
+  // on crée une methode pour vérifier si l'utilisateur est connecter
+  // cela nous permet de vérifier n'importe ou dans notre site si notre utilisateur est connecter
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+
+  // paramettre de la déconnexion
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <div>
       <div className="app-header">
-        <ConnectModal />
+        {/* la on lui demande si user existe alors si oui mais moi sa */}
+        {user && (
+          <div className="user-infos">
+            <span>{user?.displayName[0]}</span>
+            <h4>{user?.displayName}</h4>
+            <button onClick={() => handleLogout()}>
+              <i className="fa-solid fa-arrow-right-from-bracket"></i>
+            </button>
+          </div>
+        )}
+        {/* on crée une condition d'afficher qui va demander si user existe et biensur connecter affiche moi cela sinon affiche connexion ou inscription */}
+        {user ? <CreatePost /> : <ConnectModal />}
       </div>
       <div className="posts-container"></div>
     </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,21 +1,29 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { auth } from "../utils/firebase.config";
 
 const SignUp = () => {
   // On va récuperer ce qui est taper dans les inputs cela servira pour l'envoie en bdd à firebase
   const registerEmail = useRef();
   const registerPassword = useRef();
+  const [displayName, setDisplayName] = useState("");
 
   const handleRegister = (e) => {
     e.preventDefault();
 
     // on va crée la logique pour que quand l'utilisateur s'inscrie cela permettra à ce que on envoie les information en bdd
     // cela en appelant auth va nous permettre d'envoyer les information àla base de donnée de firebase
+    // dans le try on va mettre pour passer les donnée du pseudo
     try {
-      auth.createUserWithEmailAndPassword(
-        registerEmail.current.value,
-        registerPassword.current.value
-      );
+      auth
+        .createUserWithEmailAndPassword(
+          registerEmail.current.value,
+          registerPassword.current.value
+        )
+        .then(async (userAuth) => {
+          await userAuth.user.updateProfile({
+            displayName,
+          });
+        });
     } catch (error) {
       console.log(error.message);
     }
@@ -28,7 +36,12 @@ const SignUp = () => {
       <div className="signup">
         <h3>S'inscrire</h3>
         <form onSubmit={(e) => handleRegister(e)}>
-          <input type="text" placeholder="Pseudo" required />
+          <input
+            type="text"
+            placeholder="Pseudo"
+            required
+            onChange={(e) => setDisplayName(e.target.value)}
+          />
           <input
             type="email"
             placeholder="Email"
